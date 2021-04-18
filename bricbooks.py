a02ce9eb3199be777d5a6571e0098aae50e5e8c0
Ben <bcail@speedymail.org>
2021-04-17 20:45:30 -0400

    set version to 0.1

--- bricbooks.py
+++ bricbooks.py
@@ -23,7 +23,7 @@ except ImportError:
     readline = None
 
 
-__version__ = '0.1.dev'
+__version__ = '0.1'
 TITLE = f'bricbooks {__version__}'
 PYSIDE2_VERSION = '5.15.1'
 CUR_DIR = Path(__file__).parent.resolve()
