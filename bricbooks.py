0acbf7d18f7c027c8968419bd39fbf8a227918df
Ben <bcail@speedymail.org>
2022-02-16 20:10:53 -0500

    version 0.4

--- bricbooks.py
+++ bricbooks.py
@@ -23,7 +23,7 @@ except ImportError:
     readline = None
 
 
-__version__ = '0.4.dev'
+__version__ = '0.4'
 TITLE = f'bricbooks {__version__}'
 PYSIDE2_VERSION = '5.15.2'
 PYSIDE6_VERSION = '6.2.1'
