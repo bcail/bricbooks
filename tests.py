9c2885806cf71dc6d145c87ab51ecb8a0d865e39
Ben <bcail@speedymail.org>
2022-04-15 14:30:21 -0400

    gui-tk: add test for displaying account with smp unicode char

--- tests.py
+++ tests.py
@@ -6,11 +6,13 @@ import os
 import shutil
 import sqlite3
 import tempfile
+import tkinter
 import time
 import unittest
 from unittest.mock import patch, MagicMock
 
 import bricbooks as bb
+import bricbooks_tk as bb_tk
 import load_test_data
 
 
@@ -2601,6 +2603,60 @@ class TestQtGUI(unittest.TestCase):
         self.assertEqual(scheduled_txns[0].splits[housing], {'amount': 15, 'quantity': 15})
 
 
+def destroy_default_root():
+    if getattr(tkinter, '_default_root', None):
+        tkinter._default_root.update_idletasks()
+        tkinter._default_root.destroy()
+        tkinter._default_root = None
+
+class AbstractTkTest:
+
+    @classmethod
+    def setUpClass(cls):
+        cls._old_support_default_root = tkinter._support_default_root
+        destroy_default_root()
+        tkinter.NoDefaultRoot()
+        cls.root = tkinter.Tk()
+        cls.wantobjects = cls.root.wantobjects()
+        # De-maximize main window.
+        # Some window managers can maximize new windows.
+        cls.root.wm_state('normal')
+        try:
+            cls.root.wm_attributes('-zoomed', False)
+        except tkinter.TclError:
+            pass
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.root.update_idletasks()
+        cls.root.destroy()
+        del cls.root
+        tkinter._default_root = None
+        tkinter._support_default_root = cls._old_support_default_root
+
+    def setUp(self):
+        self.root.deiconify()
+
+    def tearDown(self):
+        for w in self.root.winfo_children():
+            w.destroy()
+        self.root.withdraw()
+
+
+class TestTkGUI(AbstractTkTest, unittest.TestCase):
+
+    def test_accounts_display(self):
+        gui = bb_tk.GUI_TK(':memory:')
+        checking = get_test_account()
+        savings = get_test_account(name='Savings')
+        gui._engine.save_account(account=checking)
+        gui._engine.save_account(account=savings)
+        gui.accounts_button.invoke()
+        child_items = gui.main_frame.get_children()
+        first_account_name = gui.main_frame.item(child_items[0])['values'][2]
+        self.assertEqual(first_account_name, CHECKING)
+
+
 class TestLoadTestData(unittest.TestCase):
 
     def test_load(self):
@@ -2668,11 +2724,8 @@ if __name__ == '__main__':
     import sys
     print(sys.version)
     print(f'sqlite3: {sqlite3.sqlite_version_info}')
-    try:
-        import tkinter
-        print(f'TkVersion: {tkinter.TkVersion}; TclVersion: {tkinter.TclVersion}')
-    except Exception as e:
-        print(f'Error getting Tkinter versions: {e}')
+    print(f'TkVersion: {tkinter.TkVersion}; TclVersion: {tkinter.TclVersion}')
+
     import argparse
     parser = argparse.ArgumentParser()
     parser.add_argument('--no-gui', dest='no_gui', action='store_true')
