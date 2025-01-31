68349481ba1cb684f8d3c90ef3413c6da3fdaee7
Ben <bcail@speedymail.org>
2025-01-30 20:56:37 -0500

    gui: combobox - limit filtering to printable characters and backspace

--- bricbooks.py
+++ bricbooks.py
@@ -2678,7 +2678,7 @@ class Combobox:
                     else:
                         new_cur = current_selection[0]+1
                     self.listbox.selection_set(new_cur)
-            else:
+            elif e.keysym_num < 60000 or e.keysym_num == 65288: # 65288 is <BackSpace>
                 self.filter()
 
                 if state != 'withdrawn':
